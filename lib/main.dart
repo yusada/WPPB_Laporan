3e772b4e12ca712c525b06b8903599fbb15f36a2
yusada <yus39adalmahmudi@gmail.com>
2022-03-04 14:33:14 +0700

    Percobaan 5

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Contacts',
-      theme: ThemeData(),
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        primarySwatch: Colors.blue,
+      ),
       home: const DetailScreen(),
     );
   }
